d88c24e69390022376ddc4bf9aadff1749ea7181
lvm-1896 <91154120+lvm-1896@users.noreply.github.com>
2023-09-16 23:45:54 +0200

    feat(packages) initial version

---
+++ src/packages/README.md
@@ -0,0 +1,24 @@
+
+# My Favorite Color (color)
+
+A feature to remind you of your favorite color
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/lvm-1896/features-dshr/cs-repo:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| favorite | Choose your favorite color. | string | red |
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/lvm-1896/features-dshr/blob/main/src/color/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/packages/install.sh
@@ -0,0 +1,112 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'packages'"
+echo "The provided DEBs are: ${DEBS}"
+
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final 
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
+
+set -eux
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+# rm -f /etc/profile.d/00-restore-env.sh
+# echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+# chmod +x /etc/profile.d/00-restore-env.sh
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+sudo_if() {
+    COMMAND="$*"
+    if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
+        su - "$USERNAME" -c "$COMMAND"
+    else
+        "$COMMAND"
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+##########################
+#  Install starts here   #
+##########################
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+
+apt_get_update
+
+# Install X11, fluxbox and VS Code dependencies
+check_packages ${DEBS}
+
+# Check at least one locale exists
+# if ! grep -o -E '^\s*en_US.UTF-8\s+UTF-8' /etc/locale.gen > /dev/null; then
+#     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+#     locale-gen
+# fi
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+cat <<EOF
+
+
+You now have a working ${DEBS} installed! Have fun install.sh
+
+(*) Done!
+
+EOF
